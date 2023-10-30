01151ea8a9ef4580e38b42aa45c86c30fdacf475
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-10-30 19:11:27 +0200

    Result of part "Data types"

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-// ========== Names
+// // ========== Names
+//
+// // const 2myNum = 5 // error
+//
+//
+// // const -myNum = 5  // error
+// // const my-Num = 5  // error
+// // const myNum- = 5  // error
+//
+// // const 2myNum = 5 // error
+// const my2Num = 5 // ok
+// const myNum2 = 5 // ok
+//
+// const $myNum = 5 // ok
+// const my$Num = 5 // ok
+// const myNum$ = 5 // ok
+//
+// const _myNum = 5 // ok
+// const my_Num = 5 // ok
+// const myNum_ = 5 // ok
 
-// const 2myNum = 5 // error
 
 
-// const -myNum = 5  // error
-// const my-Num = 5  // error
-// const myNum- = 5  // error
+// ========= Data Types
 
-// const 2myNum = 5 // error
-const my2Num = 5 // ok
-const myNum2 = 5 // ok
+let x
 
-const $myNum = 5 // ok
-const my$Num = 5 // ok
-const myNum$ = 5 // ok
+console.log(x) // undefined
 
-const _myNum = 5 // ok
-const my_Num = 5 // ok
-const myNum_ = 5 // ok
+console.log(1 / x) // NaN
+
+console.log(typeof (1 / x)) // !!! number
+
+console.log(typeof NaN) // !!! number
+
+
