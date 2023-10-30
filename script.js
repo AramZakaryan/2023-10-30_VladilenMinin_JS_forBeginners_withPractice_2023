cc1c8f91b333ba30f11e7e9eeade89303d2df4db
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-10-30 19:09:30 +0200

    Result of part "Names"

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// ========== Names
+
+// const 2myNum = 5 // error
 
 
+// const -myNum = 5  // error
+// const my-Num = 5  // error
+// const myNum- = 5  // error
 
+// const 2myNum = 5 // error
+const my2Num = 5 // ok
+const myNum2 = 5 // ok
+
+const $myNum = 5 // ok
+const my$Num = 5 // ok
+const myNum$ = 5 // ok
+
+const _myNum = 5 // ok
+const my_Num = 5 // ok
+const myNum_ = 5 // ok
