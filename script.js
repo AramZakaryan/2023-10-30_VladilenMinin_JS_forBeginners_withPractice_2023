ff600b5b586a2eaa8c4a877d766222f3ca193773
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-10-30 20:07:13 +0200

    Result of part "Arrays and Objects part 2"

--- script.js
+++ script.js
@@ -20,7 +20,6 @@
 // const myNum_ = 5 // ok
 
 
-
 // // ========= Data Types
 //
 // let x
@@ -34,16 +33,41 @@
 // console.log(typeof NaN) // !!! number
 
 
+// // ========== Arrays and Objects
+// // ========== part 1 
+//
+//
+// const myArr = [3, 4, 88, 0, 44]
+//
+// myArr[myArr.length]=88
+//
+// console.log(myArr)
 
-// ========== Arrays and Objects 
-// ========== part 1 
 
+// // ========== Arrays and Objects 
+// // ========== part 1 
+//
+// document.querySelector("#btn1_1").onclick= function () {
+//     document.querySelector("#div1").innerHTML = 
+//         document.querySelector("#inp1").value
+// }
+//
+// document.querySelector("#btn1_2").onclick= function () {
+//     document.querySelector("#div1").insertAdjacentHTML("beforeend",
+//     "cucu")
+//        
+// }
+
+
+// // ========== Arrays and Objects 
+// // ========== part 2
 
 const myArr = [3, 4, 88, 0, 44]
 
-myArr[myArr.length]=88
+
+for (let el of myArr) {
+    el *= 10
+}
 
 console.log(myArr)
-
-
 
